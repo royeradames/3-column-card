7e5b66cd9bde3385a225018fe2b92af6be0c4b00
royeradames <royeraadames@gmail.com>
2021-04-06 08:53:33 -0400

    📦add display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import "./styles/_root.scss"
+import React from "react";
+import "./styles/_root.scss";
 
+const data = [
+  {
+    brand: "Sedans",
+    message:
+      "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+  },
+  {
+    brand: "SUVs",
+    message:
+      "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+  },
+  {
+    brand: "Luxury",
+    message:
+      "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+  },
+];
 function App() {
-  return (
-    <></>
-  );
+  return <></>;
 }
 
 export default App;
